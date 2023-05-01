439d211f65f167f6cd64bf1290c3e7a1996848f3
ptokos <jiseokchoij@gmail.com>
2023-05-01 13:39:52 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 최지석 on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
